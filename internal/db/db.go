e845c5449d5ecb42dd99a7b3540a745c767cf55c
shramanb113 <shramanb1@gmail.com>
2026-01-17 00:37:20 +0530

    made db.go connection

---
+++ internal/db/db.go
@@ -0,0 +1,40 @@
+package db
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func connect(databaseURL string) (*pgxpool.Pool, error) {
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	cfg, err := pgxpool.ParseConfig(databaseURL)
+
+	if err != nil {
+		return nil, err
+	}
+
+	cfg.MaxConns = 10
+	cfg.MinConns = 2
+	cfg.MaxConnIdleTime = 5 * time.Minute
+	cfg.HealthCheckPeriod = 1 * time.Minute
+
+	pool, err := pgxpool.NewWithConfig(ctx, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := pool.Ping(ctx); err != nil {
+		pool.Close()
+		return nil, err
+	}
+	log.Println("✅ Database connected successfully")
+
+	return pool, nil
+}
