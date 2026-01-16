3f5ab7570984c8ccb1bcfd56f4fc75876b435700
shramanb113 <shramanb1@gmail.com>
2026-01-17 00:17:57 +0530

    added templ setup

---
+++ cmd/server/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/a-h/templ"
+	"github.com/shramanb113/websocket-challenge/views"
+)
+
+func main() {
+	// We call views.Layout("YourName") to create the component
+	// then templ.Handler() wraps it so http.ListenAndServe can use it.
+
+	http.Handle("/", templ.Handler(views.Layout("Lead Developer")))
+
+	fmt.Println("🚀 Server starting at http://localhost:8080")
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
